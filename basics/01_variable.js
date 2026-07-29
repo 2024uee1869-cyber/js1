f1e7380ce220de4815da71f4ad7b4ffa4385cbef
NISHU YADAV <230566793+2024uee1869-cyber@users.noreply.github.com>
2026-07-29 08:46:49 +0000

    datatypes

---
+++ basics/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail="nishu@google.com"
+var accountPassword="12345"
+accounntCity= "jaipur"
+let accountState;
+
+// accountId=2
+
+
+accountEmail="yadav@google.com"
+accountPassword="34567"
+accounntCity= "delhi"
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue  in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accounntCity,accountState]);
+
+
+
---
+++ basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all Js as a new version;
+
+// alert(3+3)  we are using nodejs, not browser
+
+console.log(3
+    +3) 
+//  code readability should be high
+
+console.log("hitesh")
+
+let name="hitesh"
+let age=18;
+let isLoggedIn=false
+let state;
+
+//number>= 2 to power 53
+//bigInt
+// string=>""
+// boolean=> true/false
+//null=> standalone value
+//undefined=>
+// symbol => unique
+// object
+console.log(typeof undefined); //undefined
+console.log(typeof null);  // object
+
