6ab422af851aeadf58d4df9924acfcfac727be90
NISHU YADAV <230566793+2024uee1869-cyber@users.noreply.github.com>
2026-08-01 08:39:55 +0000

    datatype summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// primitive
+// 7 types: string, Number,Boolean, NUll ,undefines, Symbol,BigInt;
+
+const scorem=100;
+const scorevalue=100.3
+const score=false
+const outsideTemp=null
+let useremail;
+const Id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(Id==anotherId);
+const nigNumber=23456789005757n
+
+
+
+
+// Refernce (Non primitive);
+
+// Arrray, Objects, Functions
+const heros=["saktiman","naagraj","doga"];
+const myObj={
+    name: "hitesh",
+    age: 22,
+
+};
+const myFunction= function(){
+    console.log("hello world");
+}
+console.log(typeof scorevalue);
+console.log(typeof myFunction);
+
+
